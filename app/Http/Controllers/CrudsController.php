c47f02a4410e3e4265d70d87ad6c4557458a7d63
HiKangg <kangg1987@hotmail.com>
2018-10-16 19:00:29 -0700

    add CrudController and Model.

---
+++ app/Http/Controllers/CrudsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Crud;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Faker\Generator;
+
+class CrudsController extends Controller
+{
+    public function create(Generator $faker)
+    {
+    	$crud = new Crud();
+    	$crud->name = $faker->lexify('????????');
+	  	$crud->color = $faker->boolean ? 'red' : 'green';
+	  	$crud->save();
+
+  		return response($crud->jsonSerialize(), Response::HTTP_CREATED);
+    }
+}
